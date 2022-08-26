e4274a54ec2a0fe5b75a86dc2a55f445584e3c0f
ftptpf <inboxru@yandex.ru>
2022-08-26 10:00:46 +0300

    Модели и связи. Машины и владельцы [#4744] - Entities

---
+++ db/scripts/001_ddl_create_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS engines (
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS cars (
+    id SERIAL PRIMARY KEY ,
+    engine_id INT NOT NULL UNIQUE REFERENCES engines(id)
+);
+
+CREATE TABLE IF NOT EXISTS drivers (
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS history_owner (
+    id SERIAL PRIMARY KEY ,
+    driver_id INT NOT NULL REFERENCES drivers(id) ,
+    car_id INT NOT NULL REFERENCES cars(id)
+);
---
+++ src/main/java/ru/job4j/model/Car.java
@@ -0,0 +1,63 @@
+package ru.job4j.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @OneToOne
+    private Engine engine;
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "history_owner", joinColumns = {
+            @JoinColumn(name = "driver_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {
+            @JoinColumn(name = "car_id", nullable = false, updatable = false)})
+    private Set<Driver> drivers = new HashSet<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Set<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public void setDrivers(Set<Driver> drivers) {
+        this.drivers = drivers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return id == car.id && Objects.equals(engine, car.engine);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, engine);
+    }
+}
