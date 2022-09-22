7c4cd3fdb7e2d2b0eb1bc9225373abed4ee31d9c
ftptpf <inboxru@yandex.ru>
2022-09-22 09:51:08 +0300

    2. Тестовое задание. Hibernate. [#330581] - CrudRepository

--- src/main/java/ru/job4j/persistence/UserDbStore.java
+++ src/main/java/ru/job4j/persistence/UserDbStore.java
@@ -1,39 +1,38 @@
 package ru.job4j.persistence;
 
-import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.User;
+import ru.job4j.util.CrudRepository;
 import ru.job4j.util.DbConnect;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class UserDbStore {
 
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
-    public UserDbStore(SessionFactory sf) {
-        this.sf = sf;
+    public UserDbStore(CrudRepository crudRepository) {
+        this.crudRepository = crudRepository;
     }
 
     public Optional<User> create(User user) {
-        try {
-            DbConnect.tx(session -> session.save(user), sf);
-        } catch (HibernateException e) {
-            e.printStackTrace();
-            return Optional.empty();
-        }
-        return Optional.of(user);
+        crudRepository.run(session -> session.persist(user));
+        return Optional.ofNullable(user);
     }
 
     public List<User> findByLoginAndPassword(String email, String password) {
-        return DbConnect.tx(session -> session.createQuery(
+/*        return DbConnect.tx(session -> session.createQuery(
                         "FROM User WHERE email = :userEmail AND password = :userPassword")
                 .setParameter("userEmail", email)
                 .setParameter("userPassword", password)
-                .list(), sf);
+                .list(), sf);*/
+        return crudRepository.query("FROM User WHERE email = :userEmail AND password = :userPassword",
+                User.class,
+                Map.of("userEmail", email, "userPassword", password)
+        );
     }
 
 }
