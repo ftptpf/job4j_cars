e3e210266ec2e43d0c4355231f3cd0cfb9431d77
ftptpf <inboxru@yandex.ru>
2022-09-28 09:31:24 +0300

    2. Тестовое задание. Hibernate. [#330581] - DbConnect test

--- src/main/java/ru/job4j/persistence/UserDbStore.java
+++ src/main/java/ru/job4j/persistence/UserDbStore.java
@@ -1,8 +1,11 @@
 package ru.job4j.persistence;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.User;
 import ru.job4j.util.CrudRepository;
+import ru.job4j.util.DbConnect;
 
 import java.util.List;
 import java.util.Map;
@@ -11,7 +14,7 @@ import java.util.Optional;
 @Repository
 public class UserDbStore {
 
-    private final CrudRepository crudRepository;
+/*    private final CrudRepository crudRepository;
 
     public UserDbStore(CrudRepository crudRepository) {
         this.crudRepository = crudRepository;
@@ -23,11 +26,45 @@ public class UserDbStore {
     }
     // TODO Optional exception when user exist
 
+    public List<User> findAll() {
+        return crudRepository.query("FROM User", User.class);
+    }
+
     public List<User> findByLoginAndPassword(String email, String password) {
         return crudRepository.query("FROM User WHERE email = :userEmail AND password = :userPassword",
                 User.class,
                 Map.of("userEmail", email, "userPassword", password)
         );
+    }*/
+
+    private final SessionFactory sf;
+
+    public UserDbStore(SessionFactory sf) {
+        this.sf = sf;
+    }
+
+    public Optional<User> create(User user) {
+        try {
+            DbConnect.tx(session -> session.save(user), sf);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+        return Optional.of(user);
+    }
+
+    public List<User> findAll() {
+        return DbConnect.tx(session -> session.createQuery(
+                "FROM User")
+                .list(), sf);
+    }
+
+    public List<User> findByLoginAndPassword(String email, String password) {
+        return DbConnect.tx(session -> session.createQuery(
+                        "FROM User WHERE email = :userEmail AND password = :userPassword")
+                .setParameter("userEmail", email)
+                .setParameter("userPassword", password)
+                .list(), sf);
     }
 
 }
---
+++ src/test/java/ru/job4j/persistence/UserDbStoreTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.persistence;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.internal.SessionFactoryImpl;
+import org.junit.jupiter.api.Test;
+import ru.job4j.model.User;
+import ru.job4j.util.CrudRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDbStoreTest {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+    private final SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+    @Test
+    public void whenAddNewUser() {
+        //UserDbStore store = new UserDbStore(new CrudRepository(sf));
+        UserDbStore store = new UserDbStore(sf);
+        User user = new User();
+        user.setName("Oleg");
+        user.setEmail("oler@oleg.ru");
+        user.setPassword("oleg");
+
+        store.create(user);
+        List<User> userList = store.findAll();
+
+        assertThat(userList)
+                .isNotEmpty()
+                .hasSize(1);
+
+    }
+
+
+}
